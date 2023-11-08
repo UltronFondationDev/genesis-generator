fbe6e1eb58737db2de38319d4058c6ec711e9d9f
semenlobojcenko <semyonl1995@gmail.com>
2023-11-08 18:14:42 +0200

    fixes

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ run command `make` to build application
 Application has following commands:
 
 1. command `./build/genesis-generator gengenesis --config=./path_to_conf --pks.path=./path_to_file_with_privatekeys --out.filename="genesis.g" (optional)` the output will be genesis file with provided name or default genesis.g
-2. command `./build/genesis-generator genkeys --count=2` the output will folder with provided count of private, pub keys and addresses
 
 # PKS File
 pks file is file with private keys for yours validators, private keys should be follow line by line, see example in ./pks.example.txt
--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/Fantom-foundation/go-opera/opera/genesisstore"
 	"github.com/UltronFoundationDev/genesis-generator/configs"
 	"github.com/UltronFoundationDev/genesis-generator/generator/genesis"
-	"github.com/UltronFoundationDev/genesis-generator/generator/pkeys"
 	"github.com/UltronFoundationDev/genesis-generator/pkeyreader"
 	cli "github.com/urfave/cli/v2"
 	"log"
 	"os"
-	"time"
 )
 
 // TODO: need to change dependencies from opera to ultron when public repo will be available
@@ -19,60 +16,6 @@ import (
 func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
-			{
-				Flags: []cli.Flag{
-					&cli.IntFlag{
-						Name:     "count",
-						Usage:    "fixes block transactions to this block",
-						Required: true,
-					},
-				},
-				Name:        "genkeys",
-				Aliases:     nil,
-				Description: "move events transactions to block transaction, required block_to flag to indicate which block should be fixed",
-				Action: func(context *cli.Context) error {
-					count := context.Int("count")
-					var pkeyS []*pkeys.PKey
-
-					for i := 1; i <= count; i++ {
-						pkeysAndress, err := pkeys.GenerateKeyAndAddress()
-						if err != nil {
-							return cli.Exit(err, 1)
-						}
-
-						pkeyS = append(pkeyS, pkeysAndress)
-					}
-
-					folderName := fmt.Sprintf("./keys_%d", time.Now().Unix())
-					if err := os.Mkdir(folderName, os.ModePerm); err != nil {
-						return cli.Exit(err, 1)
-					}
-
-					for i, pkey := range pkeyS {
-						iter := i
-						filenamepubkey := fmt.Sprintf("%s/pubkey_%d.txt", folderName, iter)
-						filenameprivkey := fmt.Sprintf("%s/privkey_%d.txt", folderName, iter)
-						filenameaddr := fmt.Sprintf("%s/addr_%d.txt", folderName, iter)
-
-						err := writeStringToFile(filenamepubkey, pkey.Public)
-						if err != nil {
-							return cli.Exit(err, 1)
-						}
-
-						err = writeStringToFile(filenameprivkey, pkey.Private)
-						if err != nil {
-							return cli.Exit(err, 1)
-						}
-
-						err = writeStringToFile(filenameaddr, pkey.Address)
-						if err != nil {
-							return cli.Exit(err, 1)
-						}
-					}
-
-					return nil
-				},
-			},
 			{
 				Flags: []cli.Flag{
 					&cli.StringFlag{
@@ -137,19 +80,4 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func writeStringToFile(fName string, content string) error {
-	fi, err := os.OpenFile(fName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	if err != nil {
-		return err
-	}
-	defer fi.Close()
-
-	_, err = fi.WriteString(content)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
